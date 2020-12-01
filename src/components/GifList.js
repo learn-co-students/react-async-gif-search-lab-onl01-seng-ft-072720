996c7a000bedcaf82baeb69514f054a181cc5946
aleebrady <aleebrady@gmail.com>
2020-12-01 11:39:05 -0800

    Done.

---
+++ src/components/GifList.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+const GifList = ({gifs}) => {
+    return(
+        <div>
+            <ul>
+                {{gifs}.map(gif => <li><img key={gif.id} src={gif.url} alt="gif" /></li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default GifList
